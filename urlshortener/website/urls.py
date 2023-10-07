5d6e9e77b9798ade436a3ae46f61982b65057763
George Hlongwane <georgehlongwane8@gmail.com>
2023-10-07 03:24:42 +0200

    Created link creation view, and got a positive http response from trying to shorten the link

--- urlshortener/website/urls.py
+++ urlshortener/website/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'), 
+    # the url we go to when the "shorten" button has been clicked
+    path('create', views.index, name='create')
 ]
--- urlshortener/website/views.py
+++ urlshortener/website/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
+import uuid
+from .models import Url
+from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
 
+# the view we get when we click the link
+def create(request):
+    if request.method == 'POST':
+        url = request.POST['link'] # take the URL given to you by the user
+        uid = str(uuid.uuid4())[:5] # give the URL an id that will be a maximum of 6 characters
+        new_url = Url(link=url, uuid=uid)
+        new_url.save()
+        return HttpResponse(uid)
