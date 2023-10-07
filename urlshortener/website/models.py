f6f23f6617163d1141f84e09610d82cc42f6fa21
George Hlongwane <georgehlongwane8@gmail.com>
2023-10-07 02:27:01 +0200

    Created URL model and superuser

--- urlshortener/website/models.py
+++ urlshortener/website/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Url(models.Model):
+    link = models.CharField(max_length=10000) # the link we give to the page form
+    uuid = models.CharField(max_length=10) # the specific id we give to the link above
+
